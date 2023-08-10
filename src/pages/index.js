b799b7b51f52ef66069f14a1ec969b4ed119f66b
Ashutosh <ashutosh.kumar2@vvdntech.in>
2023-08-10 12:04:56 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import About from './AboutPage'
+import Home from './HomePage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import Product from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Private from './PrivateRoute'
+
+export { About, Home, Cart, Checkout, Error, Product, SingleProduct, Private }
